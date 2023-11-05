7397647af712c8fe8d568579031ae738d2322174
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-04 22:29:18 -0600

    Progress on client.

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import AppProps from "next/app"
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps } />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
