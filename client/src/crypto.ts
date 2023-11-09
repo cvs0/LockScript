ae62d345d141874ee25b13d55f40be87624ca68d
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-08 20:57:01 -0700

    Vault key generation.

--- client/src/crypto.ts
+++ client/src/crypto.ts
@@ -1,9 +1,18 @@
+import pbkdf2 from "crypto-js/pbkdf2";
 import { SHA256 } from "crypto-js";
 
 export function hashPassword(password: string) {
     return SHA256(password).toString();
 }
 
-export function generateVaultKey() {
-    
+export function generateVaultKey({
+    email, hashedPassword, salt
+}: {
+    email: string;
+    hashedPassword: string;
+    salt: string;
+}) {
+    return pbkdf2(`${email}:${hashedPassword}`, salt, {
+        keySize: 32,
+    }).toString();
 }
