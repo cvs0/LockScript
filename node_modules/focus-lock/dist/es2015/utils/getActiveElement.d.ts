fb60ac59bb85d5a5cac356d0b40d61498b2118d0
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-17 21:07:46 -0700

    Project fixes

--- node_modules/focus-lock/dist/es2015/utils/getActiveElement.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * returns current active element. If the active element is a "container" itself(shadowRoot or iframe) returns active element inside it
- * @param [inDocument]
- */
-export declare const getActiveElement: (inDocument?: Document | ShadowRoot | undefined) => HTMLElement | undefined;
