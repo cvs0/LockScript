edb43097c285831f9d655fa444aa36b870a27c0d
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-21 09:08:25 -0700

    Page description constant.

--- client/src/contants.ts
+++ client/src/contants.ts
@@ -1,3 +1,7 @@
 export const PAGE_TITLE =
   process.env.PAGE_TITLE ||
   "LockScript";
+
+export const PAGE_DESCRIPTION = 
+  process.env.PAGE_DESCRIPTION ||
+  "Best solution for password security.";
