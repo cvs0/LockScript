de486b786537735e0969d750dcc21b17e7333be2
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-12-11 08:20:09 -0700

    Added comments

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,7 @@ import LoginForm from "../components/LoginForm";
 import Vault from "../components/Vault";
 import { PAGE_DESCRIPTION, PAGE_TITLE } from "../contants";
 
+// Define the shape of a single item in the vault
 export interface VaultItem {
   website: string;
   username: string;
@@ -14,39 +15,45 @@ export interface VaultItem {
 }
 
 function Home() {
+  // State variables to manage the current step, vault data, and vault key
   const [step, setStep] = useState<"login" | "register" | "vault">("login");
   const [vault, setVault] = useState<VaultItem[]>([]);
   const [vaultKey, setVaultKey] = useState("");
 
   useEffect(() => {
+    // Retrieve stored vault data and vault key from sessionStorage on component mount
     const vault = window.sessionStorage.getItem("vault");
     const vaultKey = window.sessionStorage.getItem("vk");
 
+    // Parse and set the vault data if it exists
     if (vault) {
       try {
         const parsedVault = JSON.parse(vault);
         setVault(parsedVault);
       } catch (error) {
+        // Handle parsing errors
         console.error("Error parsing vault data:", error);
       }
-      
     }
 
+    // Set the vault key and switch to the 'vault' step if the vault key exists
     if (vaultKey) {
       setVaultKey(vaultKey);
       setStep("vault");
     }
-  }, []);
+  }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
     <div className={styles.container}>
       <Head>
+        {/* Set page title, description, and favicon */}
         <title>{PAGE_TITLE}</title>
         <meta name="description" content={PAGE_DESCRIPTION} />
-        <link rel="icon" href="/client/src/public/favicone.ico" />
+        <link rel="icon" href="/client/src/public/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        {/* Render different components based on the current step */}
         {step === "register" && (
           <RegisterForm setStep={setStep} setVaultKey={setVaultKey} />
         )}
