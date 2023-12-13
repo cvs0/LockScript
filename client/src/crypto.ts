910020fb2d7e9d13c1d52dd3a0105b2ddd46c6a1
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-12-13 08:57:18 -0700

    Encryption security fixes

--- client/src/crypto.ts
+++ client/src/crypto.ts
@@ -1,7 +1,8 @@
 import pbkdf2 from "crypto-js/pbkdf2";
-import { AES, enc } from "crypto-js";
+import { AES, enc, lib } from "crypto-js";
 import * as bcrypt from 'bcrypt';
 import { promisify } from 'util';
+import * as crypto from 'crypto';
 
 const hashAsync = promisify(bcrypt.hash);
 
@@ -40,7 +41,13 @@ export function generateVaultKey({
   hashedPassword: string;
   salt: string;
 }) {
-  return pbkdf2(`${email}:${hashedPassword}`, salt, {
+  const keySize = 32;
+  const iterations = 10000;
+
+  const keyBuffer = crypto.pbkdf2Sync(`${email}:${hashedPassword}`, salt, iterations, keySize, 'sha512');
+  const key = enc.Hex.parse(keyBuffer.toString('hex'));
+
+  return pbkdf2(key, salt, {
     keySize: 32,
   }).toString();
 }
@@ -59,18 +66,23 @@ export function decryptVault({
   vaultKey: string;
   vault: string;
 }) {
-  // Decrypt the vault using the vault key.
-  const bytes = AES.decrypt(vault, vaultKey);
+  const hexIv = vault.substr(0, 32);
+  const cipherText = vault.substr(32);
+
+  const bytes = AES.decrypt(cipherText, vaultKey, {
+    iv: lib.WordArray.create(Buffer.from(hexIv, 'hex') as any),
+    format: 'hex' as any,
+  });
+
   const decrypted = bytes.toString(enc.Utf8);
 
-  // Attempt to parse the decrypted data as JSON and extract the 'vault' property.
   try {
     return JSON.parse(decrypted).vault;
   } catch (e) {
-    // Return null if parsing fails.
     return null;
   }
 }
+
 
 /**
  * Encrypts the vault using the provided vault key.
@@ -86,6 +98,13 @@ export function encryptVault({
   vaultKey: string;
   vault: string;
 }) {
-  // Encrypt the vault using the vault key.
-  return AES.encrypt(vault, vaultKey).toString();
+  const iv = enc.Hex.parse(crypto.randomBytes(16).toString('hex'));
+  const cipherParams = AES.encrypt(vault, vaultKey, { iv });
+  
+  if (!cipherParams) {
+    throw new Error('Encryption failed');
+  }
+
+  const encryptedText = enc.Hex.stringify(iv) + cipherParams.ciphertext.toString(enc.Hex);
+  return encryptedText;
 }
